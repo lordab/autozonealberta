6d3edad2611b8c632bc4cac8a8e7fdab86d89612
sarthak abraham <sarthakabraham@gmail.com>
2025-03-31 15:05:06 -0600

    changed image location

--- src/components/ui/CarCard.jsx
+++ src/components/ui/CarCard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Box, Image, Text, Flex } from '@chakra-ui/react'
+import source from '../../images/IMG_1671.png'
 const CarCard = (...props) => {
 return(
     <div style={{maxWidth: 300, height:350, backgroundColor: '#fff', borderColor: '#D9D3D0', borderWidth: '0.1px', borderStyle: 'solid', borderRadius:'0.1px'}} >
         <Image
-        src='src/images/IMG_1671.png'
+        src={source}
         fit='contain'
         htmlHeight='180px'
         htmlWidth='300px'
