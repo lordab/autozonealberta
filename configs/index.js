ae6d244a3050e718dd01298d9abaa7004a5055ac
sarthak abraham <sarthakabraham@gmail.com>
2025-04-01 13:40:18 -0600

    changed env var

--- configs/index.js
+++ configs/index.js
@@ -1,5 +1,5 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
-const sql = neon(process.env.VITE_DRIZZLE_DATABASE_URL);
+const sql = neon(JSON.stringify(import.meta.env.VITE_DRIZZLE_DATABASE_URL));
 export const db = drizzle(sql,{schema});
